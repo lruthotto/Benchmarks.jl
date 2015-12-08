e1bace81b42815bb7898038b2bc4d5dc042f8a20
Jarrett Revels <jarrettrevels@gmail.com>
2015-12-08 16:47:37 -0500

    refactor tests for wider and more explicit coverage

---
+++ test/environment_tests.jl
@@ -0,0 +1,28 @@
+import Benchmarks
+using Base.Test
+
+############################
+# Environment Construction #
+############################
+
+env = Benchmarks.Environment()
+
+sleep(1)
+
+@test (now()-env.timestamp).value >= 1000
+@test env.julia_sha1 == Base.GIT_VERSION_INFO.commit
+@test env.os == string(OS_NAME)
+@test env.cpu_cores == CPU_CORES
+@test env.arch == string(Base.ARCH)
+@test env.machine == Base.MACHINE
+@test env.use_blas64 == Base.USE_BLAS64
+@test env.word_size == Base.WORD_SIZE
+
+####################
+# Clock Resolution #
+####################
+
+res1 = Benchmarks.estimate_clock_resolution(100_000)
+res2 = Benchmarks.estimate_clock_resolution(1)
+
+@test 50 < res1 < res2
---
+++ test/execution_tests.jl
@@ -0,0 +1,57 @@
+using Benchmarks, Base.Test
+
+####################
+# @benchmark macro #
+####################
+
+@test begin
+    # no-op #
+    #-------#
+    f() = nothing
+    @benchmark f()
+
+    # infix operators #
+    #-----------------#
+    @benchmark (3.0+5im)^3.2
+
+    # indexing #
+    #----------#
+    A = rand(2,2)
+    @benchmark A[end,end]
+
+    # keyword arguments #
+    #-------------------#
+    @benchmark svds(A, nsv=1)
+
+    # local scopes #
+    #--------------#
+    x = 1
+    let B = copy(A), y = 2
+        @benchmark B[1]
+        @benchmark x+y
+    end
+
+    true # got through without error
+end
+
+########################
+# @benchmarkable macro #
+########################
+
+# general #
+#---------#
+v = Int[0, 0]
+Benchmarks.@benchmarkable(sin_benchmark!, (v[1] += 1), sin(2.0), (v[2] += 1))
+r = Benchmarks.execute(sin_benchmark!; time_limit = 1)
+
+@test r.precompiled && r.multiple_evals
+@test 0 < r.totaltime < 1
+@test v[1] > 50 && v[1] == v[2]
+
+# time_limit #
+#------------#
+Benchmarks.@benchmarkable(sleep_benchmark!, nothing, sleep(2), nothing)
+r = Benchmarks.execute(sleep_benchmark!; time_limit = 2)
+
+@test !(r.precompiled) && !(r.multiple_evals)
+@test 2 <= r.totaltime < 2.3
