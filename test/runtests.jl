9db7de05401ccdbe7dfcd9b204f8884523e76c6b
Jarrett Revels <jarrettrevels@gmail.com>
2016-02-10 11:50:00 -0500

    add progress printlns to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,15 @@
+
+print("Testing enviroment detection...")
+tic()
 include("environment_tests.jl")
+println("done (took $(toq()) seconds).")
+
+print("Testing Samples/ExecutionResults/SummaryStats...")
+tic()
 include("results_tests.jl")
+println("done (took $(toq()) seconds).")
+
+print("Testing @benchmark/@benchmarkable/execution...")
+tic()
 include("execution_tests.jl")
+println("done (took $(toq()) seconds).")
